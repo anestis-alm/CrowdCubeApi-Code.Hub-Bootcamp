deef6993b7b72d09d53bd400c9ff0b222e19f59c
Anestis Almaliotis <anestis-almal@hotmail.com>
2020-06-11 13:41:49 +0300

    Master_Add_Views

--- CrowdCubeApp/CrowdCubeMvc/Startup.cs
+++ CrowdCubeApp/CrowdCubeMvc/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CrowdCubeApp.Repository;
+using CrowdCubeApp.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +26,17 @@ namespace CrowdCubeMvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CrowdDbContext>(options =>
+                options.UseSqlServer(CrowdDbContext.ConnectionString));
+
+            services.AddTransient<IUserManager, UserManagement>();
+            services.AddTransient<IProjectManager, ProjectManagement>();
+            services.AddTransient<IPackageManager, PackageManagement>();
+            services.AddTransient<IMediaManager, MediaManagement>();
+            services.AddTransient<IProjectStatusManager, ProjectStatusManagement>();
+
             services.AddControllersWithViews();
+            services.AddLogging();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,10 +49,7 @@ namespace CrowdCubeMvc
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
